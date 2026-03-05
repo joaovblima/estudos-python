a89cf0cac83841627d69cf839cd2cda135568c31
João Lima <joaovictorbernardinodelima@gmail.com>
2026-03-05 12:53:03 -0300

    exemplo com ints e floats

---
+++ python-crash-course/cap2/numbers.py
@@ -0,0 +1,9 @@
+#operações que resultam em 8 
+print(3 + 5)
+print(16 - 8)
+print(2 * 4)
+print(2 ** 3)
+#fazer esse comentário faz parte do meu exercicio proposto
+fav_number = 99
+message = "my nickname on the mmorpg is always " + str(99)
+print(message)
