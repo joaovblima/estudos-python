2ce6fab1e93dfa788949542c37cab3b7a4cad477
João Lima <joaovictorbernardinodelima@gmail.com>
2026-03-05 19:37:17 -0300

    add novo exemplo com listas

--- python-crash-course/cap3/motorcycles.py
+++ python-crash-course/cap3/motorcycles.py
@@ -1,12 +1,21 @@
 motorcycle = ['honda', 'yamaha', 'suzuki']
 print(motorcycle)
+#alterando elemento da primeira posiçãoo
 motorcycle[0]='ducati'
 print(motorcycle)
+#adicionando mais um elemento a lista utilizando o metodo append()
 motorcycle.append('honda')
 print(motorcycle)
+#deletando o elemento da posição 2
 del motorcycle[2]
 print(motorcycle)
+#removendo e reaproveitando o elemento usando pop()
 last_owned = motorcycle.pop()
 print("The last motorcycle I owned was a " + last_owned.title() )
+
+motorcycle = ['honda', 'yamaha', 'suzuki']
+too_expensive = 'yamaha'
+motorcycle.remove(too_expensive)
+
 
 
