b6174f5720d8fa8310790acdd3db388280029b38
joao lima <joaovictorbernardinodelima@gmail.com>
2026-03-11 18:43:19 -0300

    add glossario resolvido com dicionarios

---
+++ python-crash-course/cap6/glossario.py
@@ -0,0 +1,10 @@
+glossario = {
+    "for": "palavra reservada no python utilizada para escrever loop",
+    "if": "estrutura condicional que utilizamos uma expressao para obter um resultado False ou True",
+    "identacao": "bloco de codigo que eh utilizado para separar codigo, facilitar a leitura e manter codigo mais organizado em python",
+    "lists": "estrutura de dados que armazena n valores",
+    "dicionarios": "estrutura de dados que armazena pares de chave e valor",
+}
+
+for k, v in glossario.items():
+    print(k, "->", v)
