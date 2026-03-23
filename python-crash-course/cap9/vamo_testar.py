6a9d460b5ee4d85f6132f83aa1ac7c82d43b4b6e
João Lima <joaovictorbernardinodelima@gmail.com>
2026-03-23 15:18:29 -0300

    teste push new setup

---
+++ python-crash-course/cap9/vamo_testar.py
@@ -0,0 +1,12 @@
+
+
+def vamos_fazer_um_teste(nome):
+    if nome == "joao":
+        print("carai borracha", nome.title(), "voltou na pegada do ladrao")
+    else:
+        print("fala cachorro", nome.title())
+
+
+
+nome = input("digite seu nome companheiro: ")
+vamos_fazer_um_teste(nome)
